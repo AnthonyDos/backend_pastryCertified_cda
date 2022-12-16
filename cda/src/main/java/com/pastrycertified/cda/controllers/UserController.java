bf79ab688772eab14b671d8ea0d5b254bfe5a28f
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2022-12-16 18:44:23 +0100

    controller user and exception

--- cda/src/main/java/com/pastrycertified/cda/controllers/UserController.java
+++ cda/src/main/java/com/pastrycertified/cda/controllers/UserController.java
@@ -1,16 +1,11 @@
 package com.pastrycertified.cda.controllers;
 
-import com.pastrycertified.cda.dto.AddressDto;
 import com.pastrycertified.cda.dto.UserDto;
-import com.pastrycertified.cda.models.User;
 import com.pastrycertified.cda.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -19,12 +14,12 @@ public class UserController {
 
     private final UserService service;
 
-//    @PostMapping("/")
-//    public ResponseEntity<Integer> save(
-//            @RequestBody UserDto userDto
-//            ) {
-//        return ResponseEntity.ok(service.save(userDto));
-//    }
+    @PostMapping("/")
+    public ResponseEntity<Integer> save(
+            @RequestBody UserDto userDto
+            ) {
+        return ResponseEntity.ok(service.save(userDto));
+    }
 
     @GetMapping("/")
     public ResponseEntity<List<UserDto>> findAll() { return ResponseEntity.ok(service.findAll()); }
@@ -43,5 +38,13 @@ public class UserController {
     ) {
         System.out.println(user);
         return ResponseEntity.ok(service.updateByid(userId,user));
+    }
+
+    @DeleteMapping("/{user-id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("user-id") Integer userId
+    ) {
+        service.delete(userId);
+        return ResponseEntity.accepted().build();
     }
 }
